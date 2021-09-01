573e32d40e8bb8cdf5abf3e608ce248bebdeeccc
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-09-01 13:49:43 -0300

    Rearrange project structure

---
+++ config.py
@@ -0,0 +1,6 @@
+# To generate a new secret key:
+# >>> import random, string
+# >>> "".join([random.choice(string.printable) for _ in range(24)])
+SECRET_KEY = "vX^@1}f,k\tR\\k\x0b>\x0bVmt\"A]:|"
+
+FB_APP_ID = 995117698004144
